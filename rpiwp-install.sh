b56766461ec9753cb7b0872c4d19303fc6b300df
sondregronas <44143748+sondregronas@users.noreply.github.com>
2021-02-19 20:30:43 +0100

    Typo fixes & script now performs the wp-config for you

--- rpiwp-install.sh
+++ rpiwp-install.sh
@@ -2,7 +2,10 @@
 echo Welcome to the Wordpress One-click installer
 echo This installer creates a subdirectory for Wordpress
 
-read -p "Type your desired html folder name: " X
+read -p "Type your desired html folder name: " input
+
+target=$(echo "$input" | sed 's/\ /_/g')
+host=$(echo "$(hostname -I)" | sed 's/\ //g')
 
 # Make sure Raspbian is up to date, this part can be optional
 apt-get update -y
@@ -10,6 +13,8 @@ apt-get upgrade -y
 
 # Install necessary requirements
 apt-get install apache2 -y
+
+# These ones we should already have, keeping them here for now though
 apt-get install php-mysql -y
 apt-get install php -y
 apt-get install mariadb-server -y
@@ -17,8 +22,8 @@ apt-get install mariadb-server -y
 cd /var/www/html/
 
 # This part is optional, it's responsible for creating subdirectories for additional websites per machine
-mkdir $X
-cd $X
+mkdir $target
+cd $target
 
 wget http://wordpress.org/latest.tar.gz
 tar xzf latest.tar.gz
@@ -31,18 +36,25 @@ chown -R www-data: .
 printf "\n n\n y\n y\n y\n y\n" | sudo mysql_secure_installation
 
 # Create a database & user with the X variable. Password is set to 'password', this is the default password for Wordpress
-echo "create database $X;" | mysql -uroot
-echo "CREATE USER '$X'@'localhost' IDENTIFIED BY 'password';" | mysql -uroot
-echo "GRANT ALL ON $X.* TO '$X'@'localhost';" | mysql -uroot
+echo "create database $target;" | mysql -uroot
+echo "CREATE USER '$target'@'localhost' IDENTIFIED BY 'password';" | mysql -uroot
+echo "GRANT ALL ON $target.* TO '$target'@'localhost';" | mysql -uroot
 echo "FLUSH PRIVILEGES;" | mysql -uroot
 
 # Finishing touches
 sudo service apache2 restart
 
-echo ----------------------------------
-echo ----------------------------------
-echo All done, use the Database and Username: $X, 
-echo when setting up your installation at $(hostname -I)/$X
-echo Leave the password as password, and use localhost as the host
-echo ----------------------------------
-echo ----------------------------------
+# Configuring wordpress
+cp wp-config-sample.php wp-config.php
+sed -i "s/database_name_here/$target/g" wp-config.php
+sed -i "s/username_here/$target/g" wp-config.php
+sed -i "s/password_here/password/g" wp-config.php
+
+###########
+# TODO: Send a URL to the Admin Pi & update the adminpi/index.html
+###########
+
+printf "\n\n\n"
+echo - - - - - - - - - - - - - - - - - -
+echo All done, your site is now waiting for you at http://$host/$target
+echo - - - - - - - - - - - - - - - - - -
